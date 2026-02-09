5da4a637bd7ebf9fce2306cbfc5abb70772e5377
qndree <danisuarezclaudino013@gmail.com>
2026-02-08 18:16:38 -0600

    Avance de guia de ejercicios

---
+++ ejercicios/Ejercicio01/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("=== Calculadora de IMC ===");
+
+        Console.Write("Ingrese su peso en kg: ");
+        double peso = double.Parse(Console.ReadLine());
+
+        Console.Write("Ingrese su altura en metros: ");
+        double altura = double.Parse(Console.ReadLine());
+
+        double imc = peso / (altura * altura);
+
+        Console.WriteLine("\nSu IMC es: " + imc.ToString("F2"));
+
+        if (imc < 18.5)
+            Console.WriteLine("Categoría: Bajo peso");
+        else if (imc < 25)
+            Console.WriteLine("Categoría: Normal");
+        else if (imc < 30)
+            Console.WriteLine("Categoría: Sobrepeso");
+        else
+            Console.WriteLine("Categoría: Obesidad");
+    }
+}
